2f09fdf2c6334a2d9bdb27e8dce29ded91492a01
Kris De Smet <40060528+KDeSmet@users.noreply.github.com>
2019-06-12 23:15:28 +0200

    update help and examples

--- jdss-api-tools.py
+++ jdss-api-tools.py
@@ -475,20 +475,19 @@ def get_args(batch_args_line=None):
     {LG}%(prog)s move --pool Pool-0 --node 192.168.0.82{ENDF}
 
 
-{} {BOLD}Create storage resource{END}. Creates iSCSI target with volume (zvol) or SMB share with dataset.
+{} {BOLD}Create storage resource{END}. Creates iSCSI target with volume (zvol) or SMB/NFS share with dataset.
 
     Defaults are: size = 1TB, provisioning = thin, volume = auto, target = auto, share_name = auto.
     Example for iSCSI target with specified volume, target, size and provisioning.
 
-    {LG}%(prog)s create_storage_resource --pool Pool-0 --storage_type iscsi --volume zvol00 --target iqn.2018-09:ha-00.target0 --size 1TB --provisioning thin --node 192.168.0.220{ENDF}
+    {LG}%(prog)s create_storage_resource --pool Pool-0 --storage_type iscsi --volume zvol00 --target iqn.2018-09:target0 --size 1TB --provisioning thin --node 192.168.0.220{ENDF}
 
-    If target = auto (default), the cluster name "ha-00" will be used in the auto-target_name.
-    In the next example target name will also be "iqn.2018-09:ha-00.target0".
-    If "iqn.2018-09:ha-00.target0" and "zvol00" already exist, program will use next one: "iqn.2018-09:ha-00.target1" and "zvol01".
+    If cluster name is specified, it will be used in the target name. Next examples will create both the same target name.
 
     {LG}%(prog)s create_storage_resource --pool Pool-0 --storage_type iscsi --cluster ha-00 --node 192.168.0.220{ENDF}
+    {LG}%(prog)s create_storage_resource --pool Pool-0 --storage_type iscsi --volume zvol00 --target iqn.2018-09:target0 --cluster ha-00 --node 192.168.0.220{ENDF}
 
-    With missing --cluster ha-00, it will produce same result as "ha-00" is default cluster name.
+    With missing --target argument, it will produce auto-target name based on the host name.
 
     {LG}%(prog)s create_storage_resource --pool Pool-0 --storage_type iscsi --node 192.168.0.220{ENDF}
 
@@ -824,8 +823,6 @@ download and install "Microsoft Visual C++ 2010 Redistributable Package (x86)": 
     parser.add_argument(
         '--cluster',
         metavar='name',
-        #default='ha-00',
-        #help='Enter the cluster name, default=ha-00'
         help='Enter the cluster name'
     )
     parser.add_argument(
